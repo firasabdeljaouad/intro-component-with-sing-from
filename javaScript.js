7da70e4549688201069b1eeb1ec307419194de5f
firas <firasabdeljaouad@gmail.com>
2025-02-28 19:17:22 +0100

    commit 2

--- javaScript.js
+++ javaScript.js
@@ -4,14 +4,12 @@ let inputs = document.querySelectorAll("input:not([type='submit'])");
 
 inputs.forEach((input) => {
     input.addEventListener('invalid', addErrorMessage);
-    input.addEventListener('blur', () => {
-        input.checkVisibility();
-    })
+    input.addEventListener('change', removeTheMessage )
 
-    input.addEventListener('foucs', removeTheMessage);
 });
 
 function addErrorMessage(e) {
+    
     const name = e.target.getAttribute("name");
     let error_icon = document.createElement('span');
     error_icon.setAttribute('data-id', name)
@@ -22,19 +20,20 @@ function addErrorMessage(e) {
     error_message.classList.add("error_message");
     if (e.target.value == "" || e.target.value == null) {
         error_message.innerHTML = "" + e.target.getAttribute('placeholder') + "cannat be empty."
-    }else{
-        error_message.remove()
-    }
-    e.target.after(error_message);
-    e.target.after(error_icon);
-    e.target.classList.add('error');
+        e.target.after(error_message);
+        e.target.after(error_icon);
+        e.target.classList.add('error');
+    
+}
 }
 
 function removeTheMessage(e) {
     let elements = document.querySelectorAll("[data-id='" + e.target.getAttribute("name") + "']");
+    console.log(elements)
     elements.forEach(element => {
         element.remove();
     })
     e.target.classList.remove('error')
 }
+
 
